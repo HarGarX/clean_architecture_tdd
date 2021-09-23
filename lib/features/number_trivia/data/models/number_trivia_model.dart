d3240f9b4cbe5bc7a6d223545e2547acdbdff59e
HarGarX <hargarx@gmail.com>
2021-09-23 11:30:51 +0200

    adding model + tojon & fromJson convert

--- lib/features/number_trivia/data/models/number_trivia_model.dart
+++ lib/features/number_trivia/data/models/number_trivia_model.dart
@@ -8,4 +8,15 @@ class NumberTriviaModel extends NumberTrivia {
     @required String text,
     @required int number,
   }) : super(number: number, text: text);
+
+  factory NumberTriviaModel.fromJson(Map<String, dynamic> json) {
+    return NumberTriviaModel(
+      text: json['text'],
+      number: (json['number'] as num).toInt(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {'text': text, 'number': number};
+  }
 }
--- test/features/number_trivia/data/models/number_trivia_model_test.dart
+++ test/features/number_trivia/data/models/number_trivia_model_test.dart
@@ -1,11 +1,52 @@
+import 'dart:convert';
+
 import 'package:clean_architecture_tdd_course/features/number_trivia/data/models/number_trivia_model.dart';
 import 'package:clean_architecture_tdd_course/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:flutter_test/flutter_test.dart';
+
+import '../../../../fixtures/fixture_reader.dart';
 
 void main() {
   final tNumberTriviaModel = NumberTriviaModel(text: 'Test Text', number: 1);
 
   test('should be a subclass of NumberTivia entity', () async {
     expect(tNumberTriviaModel, isA<NumberTrivia>());
+  });
+
+  group('FromJson', () {
+    test(
+      'should return a vaild model when the JSON number is an integer',
+      () async {
+        // arrange
+        final Map<String, dynamic> jsonMap = jsonDecode(fixture('trivia.json'));
+        //act
+        final result = NumberTriviaModel.fromJson(jsonMap);
+        //assert
+        expect(result, tNumberTriviaModel);
+      },
+    );
+    test(
+        'should return a valid model when the JSON number is regarded as a double',
+        () async {
+      final Map<String, dynamic> jsonMap =
+          json.decode(fixture('trivia_double.json'));
+      final result = NumberTriviaModel.fromJson(jsonMap);
+      expect(result, tNumberTriviaModel);
+    });
+  });
+  group('toJson', () {
+    test(
+      'should return a JSON map containing the proper data',
+      () async {
+        //act
+        final result = tNumberTriviaModel.toJson();
+        //assert
+        final expectedJsonMap = {
+          "text": "Test Text",
+          "number": 1,
+        };
+        expect(result, expectedJsonMap);
+      },
+    );
   });
 }
